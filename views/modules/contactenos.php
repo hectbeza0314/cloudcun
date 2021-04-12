a6c58516bc6b188eff89416fbf874f3254d22e0b
hectbeza0314 <82351757+hectbeza0314@users.noreply.github.com>
2021-04-11 22:00:32 -0500

    Add files via upload

---
+++ views/modules/contactenos.php
@@ -0,0 +1,90 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Alumbrados de Oriente</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href='//fonts.googleapis.com/css?family=Marcellus+SC' rel='stylesheet' type='text/css'>
+<link href="views/css/style.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+
+<!-- Inicio de contenido -->
+<div class="content_bg">
+<div class="wrap">
+<div class="wrapper">
+<div class="main">
+		<div class="section group">				
+				<div class="col span_1_of_2">
+      			<div class="company_address">
+				     	<h2>Datos de Contacto</h2>
+						<p>500 Lorem Ipsum Dolor Sit,</p>
+						<p>22-56-2-9 Sit Amet, Lorem,</p>
+						<p>USA</p>
+				   		<p>Phone:(00) 222 666 444</p>
+				   		<p>Fax: (000) 000 00 00 0</p>
+				 	 	<p>Email: <span><a href="mailto:info@example.com">mail@example.com</a></span></p>
+				   		<p>Siguenos en: <span><a href="https://www.facebook.com/">Facebook</a></span></p>
+				   </div>
+					<div class="contact_info">
+			    	 	<h2>Encuentranos Aquí</h2>
+			    	 		<div class="map">
+                             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2165.284757096686!2d-73.56844201491153!3d4.25848516943492!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3fd378f02e4635%3A0xf74056281447e8c!2sCl.%204%2C%20Restrepo%2C%20Meta%2C%20Colombia!5e0!3m2!1ses!2sus!4v1611282575814!5m2!1ses!2sus" width="550" height="230" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe><br><small><a href="https://www.google.com/maps/place/Cl.+4,+Restrepo,+Meta,+Colombia/@4.2584852,-73.568442,17.88z/data=!4m5!3m4!1s0x8e3fd378f02e4635:0xf74056281447e8c!8m2!3d4.2584632!4d-73.5678135?hl=es" style="color: #685399;text-align:left;font-size:13px">Vista Completa del Mapa</a></small>
+					   		</div>
+      				</div>				   
+				   <div class="clear"></div>
+				</div>				
+				<div class="col span_2_of_4">
+				  <div class="contact-form">
+				  	<h2>Escríbenos</h2>
+					       <form id="contact-form" class="contact" name="contact-form" method="post" action="">
+					    	<div>
+						    	<span><label>Nombre</label></span>
+						    	<span><input name="userName" type="text" class="textbox" required="required"></span>
+						    </div>
+						    <div>
+						    	<span><label>Correo Electrónico</label></span>
+						    	<span><input name="userEmail" type="text" class="textbox" required="required"></span>
+						    </div>
+						    <div>
+						     	<span><label>Teléfono</label></span>
+						    	<span><input name="userPhone" type="text" class="textbox" required="required"></span>
+						    </div>
+                            <div>
+                                <span><label>Asunto</label></span>
+                                <span>
+                                    <select name="Asunto" required="required">
+                                        <option>Selecciona una Opción</option>
+                                        <option>Petición</option>
+                                        <option>Queja</option>
+                                        <option>Reclamo</option>
+                                        <option>Sugerencia</option>
+                                        <option>Felicitación</option>
+                                    </select>
+                                </span>
+                            </div>
+						    <div>
+						    	<span><label>Mensaje</label></span>
+						    	<span><textarea name="userMsg" required="required"> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" value="Envíar"></span>
+						  </div>
+					    </form>
+
+				    </div>
+  				</div>		
+  			<div class="clear"></div>
+		  </div>
+	</div>
+</div>
+</div>
+</div>
+</body>
+</html>
